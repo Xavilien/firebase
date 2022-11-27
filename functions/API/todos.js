b9a4a434b55fd604302b0789779264248e257f9c
Xavilien <xavilien@gmail.com>
2022-11-28 02:11:43 +0800

    Initial commit. Completed Section 1

---
+++ functions/API/todos.js
@@ -0,0 +1,96 @@
+const { db } = require("../util/admin");
+
+exports.getAllTodos = (request, response) => {
+    console.log("OK")
+    db
+        .collection("todos")
+        .orderBy("createdAt", "desc")
+        .get()
+        .then((data) => {
+            let todos = [];
+            data.forEach((doc) => {
+                todos.push({
+                    todoId: doc.id,
+                    title: doc.data().title,
+                    body: doc.data().body,
+                    createdAt: doc.data().createdAt,
+                });
+            });
+            return response.json(todos);
+        })
+        .catch((err) => {
+           console.error(err);
+           return response.status(500).json({ error: err.code })
+        });
+};
+
+exports.postOneTodo = (request, response) => {
+    if (request.body.body.trim() === "") {
+        return response.status(400).json({ body: "Must not be empty" });
+    }
+    if (request.body.title.trim() === "") {
+        return response.status(400).json({ title: "Must not be empty" });
+    }
+
+    const newTodoItem = {
+        title: request.body.title,
+        body: request.body.body,
+        createdAt: new Date().toISOString()
+    }
+    db
+        .collection("todos")
+        .add(newTodoItem)
+        .then((doc) => {
+            const responseTodoItem = newTodoItem;
+            responseTodoItem.id = doc.id;
+            return response.json(responseTodoItem);
+        })
+        .catch((err) => {
+            console.error(err);
+            response.status(500).json({ error: "Something went wrong" });
+        });
+};
+
+exports.deleteTodo = (request, response) => {
+    const document = db.doc(`/todos/${request.params.todoId}`);
+    document
+        .get()
+        .then((doc) => {
+            if (!doc.exists) {
+                return response.status(404).json({ error: "Todo not found" });
+            }
+            return document.delete();
+        })
+        .then(() => {
+            response.json({ message: "Delete successful" });
+        })
+        .catch((err) => {
+            console.error(err);
+            return response.status(500).json({ error: err.code });
+        });
+};
+
+exports.editTodo = (request, response) => {
+    if (request.body.todoId || request.body.createdAt) {
+        response.status(403).json({ message: "Not allowed to edit"} );
+    }
+    // const document = db.doc(`/todos/${request.params.todoId}`);
+
+    let document = db.doc(`/todos/${request.params.todoId}`);
+
+    document
+        .get()
+        .then((doc) => {
+            if (!doc.exists) {
+                return response.status(404).json({ error: "Todo not found" });
+            }
+            return document.update(request.body)
+                .then(() => {
+                    response.json({ message: "Updated successfully"} );
+                })
+                .catch((err) => {
+                    console.error(err);
+                    return response.status(500).json({ error: err.code });
+                });
+        })
+}
