72fd7ea04b7911315475019df332194fb2f4238b
Xavilien <xavilien@gmail.com>
2022-11-28 02:44:10 +0800

    Completed Section 2.2

--- functions/index.js
+++ functions/index.js
@@ -8,16 +8,19 @@ const {
     editTodo
 } = require("./API/todos");
 
+// Todos
 app.get("/todos", getAllTodos);
 app.post("/todo", postOneTodo);
 app.delete('/todo/:todoId', deleteTodo);
 app.put("/todo/:todoId", editTodo);
 
 const {
-    loginUser
+    loginUser,
+    signUpUser
 } = require('./API/users')
 
 // Users
-app.post('/login', loginUser);
+app.post("/login", loginUser);
+app.post("/signup", signUpUser);
 
 exports.api = functions.https.onRequest(app);
--- functions/util/validators.js
+++ functions/util/validators.js
@@ -1,11 +1,43 @@
 const isEmpty = (string) => {
-    return string.trim() === '';
+    return string.trim() === "";
+};
+
+const isEmail = (email) => {
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return !!email.match(emailRegEx);
 };
 
 exports.validateLoginData = (data) => {
     let errors = {};
-    if (isEmpty(data.email)) errors.email = 'Must not be empty';
-    if (isEmpty(data.password)) errors.password = 'Must not be  empty';
+
+    // Check if either email or password is empty
+    if (isEmpty(data.email)) errors.email = "Email must not be empty";
+    if (isEmpty(data.password)) errors.password = "Password must not be  empty";
+
+    return {
+        errors,
+        valid: Object.keys(errors).length === 0
+    };
+};
+
+exports.validateSignUpData = (data) => {
+    let errors = {};
+
+    // Check if either email is valid
+    if (isEmpty(data.email)) errors.email = "Email must not be empty";
+    else if (!isEmail(data.email)) errors.email = "Must be valid email address";
+
+    // Check if other details are empty
+    if (isEmpty(data.firstName)) errors.firstName = "First name must not be empty";
+    if (isEmpty(data.lastName)) errors.lastName = "Last name must not be empty";
+    if (isEmpty(data.phoneNumber)) errors.phoneNumber = "Phone number must not be empty";
+    if (isEmpty(data.country)) errors.country = "Country must not be empty";
+
+    //
+    if (isEmpty(data.password)) errors.password = "Password must not be empty";
+    if (data.password !== data.confirmPassword) errors.confirmPassword = "Passwords must be the same";
+    if (isEmpty(data.username)) errors.username = "Must not be empty"
+
     return {
         errors,
         valid: Object.keys(errors).length === 0
