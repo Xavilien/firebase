87a6d0144cdf935b16b5fd93ddbd84e43fb250ed
Xavilien <xavilien@gmail.com>
2022-11-30 22:24:49 +0800

    Configure deployment for Github pages

--- view/src/App.js
+++ view/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
